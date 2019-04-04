3a050744fbc6b7744f9e34cd72db37c048177797
Stefan Kashner <s.kash@yahoo.com>
2019-04-04 18:35:24 -0400

    Updates

---
+++ precache-manifest.76c34b122863b3261b64cc716a0c7c17.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "0610b48f3042cc268092b5e34b7a440e",
+    "url": "/Clicky-Game/static/media/cheryl.0610b48f.jpg"
+  },
+  {
+    "revision": "2bcb88eb5d41a7bff568",
+    "url": "/Clicky-Game/static/css/main.41c80a38.chunk.css"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "a9e390e5aa2be3fc53ddd201dfb5932a",
+    "url": "/Clicky-Game/static/media/background.a9e390e5.png"
+  },
+  {
+    "revision": "855f2d1030e6658093704237e0b4b88f",
+    "url": "/Clicky-Game/static/media/archer.855f2d10.jpg"
+  },
+  {
+    "revision": "c5e58617d9a7cb24d7d662ac2aa067fe",
+    "url": "/Clicky-Game/static/media/background.c5e58617.jpg"
+  },
+  {
+    "revision": "77c9cf71810b5b5ccbc709535c1fb22e",
+    "url": "/Clicky-Game/static/media/barry.77c9cf71.jpg"
+  },
+  {
+    "revision": "1ee52bd9ef7635954ce4754c712f3338",
+    "url": "/Clicky-Game/static/media/burt.1ee52bd9.jpg"
+  },
+  {
+    "revision": "2bcb88eb5d41a7bff568",
+    "url": "/Clicky-Game/static/js/main.b2ed1ca6.chunk.js"
+  },
+  {
+    "revision": "f6630dad7cad5276ef6ec6fdd0ea2782",
+    "url": "/Clicky-Game/static/media/cyril.f6630dad.jpg"
+  },
+  {
+    "revision": "e0e89ae6c829f104327a36df57010007",
+    "url": "/Clicky-Game/static/media/katya.e0e89ae6.jpg"
+  },
+  {
+    "revision": "3de93e54e8863cedbe6b7551700b99c2",
+    "url": "/Clicky-Game/static/media/krieger.3de93e54.jpg"
+  },
+  {
+    "revision": "ebbb9dc2c9420df195e14d9c39fc77c9",
+    "url": "/Clicky-Game/static/media/lana.ebbb9dc2.jpg"
+  },
+  {
+    "revision": "8fb681591ef3b7387c61cf95e24bc8f2",
+    "url": "/Clicky-Game/static/media/malory.8fb68159.jpg"
+  },
+  {
+    "revision": "59c301121007cfcf2cb42a18a73c292a",
+    "url": "/Clicky-Game/static/media/pam.59c30112.jpg"
+  },
+  {
+    "revision": "3dd3007ee8b876780bc1f6a4d6582097",
+    "url": "/Clicky-Game/static/media/ray.3dd3007e.jpg"
+  },
+  {
+    "revision": "81afc27f53c5768da6364bc334fc59a8",
+    "url": "/Clicky-Game/static/media/woodhouse.81afc27f.jpg"
+  },
+  {
+    "revision": "dcc582a7c067f507c1e16fd434b94c5b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
